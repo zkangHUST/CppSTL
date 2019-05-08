95d8fa4b673271189d9d4f1d7f9472184c612913
vincent.zheng <vincent.zheng@zoom.us>
2019-05-08 13:42:08 +0800

    update minstack.hpp

--- minstack.hpp
+++ minstack.hpp
@@ -2,26 +2,26 @@
 #define MY_STACK_H
 #include<cassert>
 #define ERROR -1
-#define OK     0
+#define OK   0
 template <class T>
 class stack
 {
 private:
-    int         top;
-    int         minTop;
-    T           buf[1024];
-    int         minIndex[1024];
-    const int   maxsize = 1024;
+  int     top;
+  int     minTop;
+  T       buf[1024];
+  int     minIndex[1024];
+  const int   maxsize = 1024;
 public:
-    int push(const T& item);
-    int pushMinIndex(const T& item);
-    int popMinIndex();
-    T   min();
-    T   pop();
-    stack();
-    stack(const stack& s);
-    stack& operator=(const stack& s);
-    ~stack();
+  int push(const T& item);
+  int pushMinIndex(const T& item);
+  int popMinIndex();
+  T   min();
+  T   pop();
+  stack();
+  stack(const stack& s);
+  stack& operator=(const stack& s);
+  ~stack();
 };
 
 template<class T>
@@ -36,79 +36,79 @@ stack<T>::~stack() {}
 template<class T>
 int stack<T>::push(const T& item)
 {
-    top++;
-    if ((top >= maxsize) || (top < 0)) {
-        return ERROR;
-    }
-    buf[top] = item;
-    pushMinIndex(item);
-    return OK;
+  top++;
+  if ((top >= maxsize) || (top < 0)) {
+    return ERROR;
+  }
+  buf[top] = item;
+  pushMinIndex(item);
+  return OK;
 }
 
 template<class T>
 T stack<T>::pop() 
 {
-    assert(top >= 0);
-    popMinIndex();
-    return buf[top--];
+  assert(top >= 0);
+  popMinIndex();
+  return buf[top--];
 }
 
 template<class T> 
 int stack<T>::pushMinIndex(const T& item)
 {
-    minTop++;
-    if ((minTop >= maxsize) || (minTop < 0)) {
-        return ERROR;
-    }
-    if (minTop == 0) {
-        minIndex[minTop] = top;
-        return OK;
-    }
-    if (item < buf[minIndex[minTop - 1]]) {
-        minIndex[minTop] = top;
-    } else {
-        minIndex[minTop] = minIndex[minTop - 1];
-    }
+  minTop++;
+  if ((minTop >= maxsize) || (minTop < 0)) {
+    return ERROR;
+  }
+  if (minTop == 0) {
+    minIndex[minTop] = top;
     return OK;
+  }
+  if (item < buf[minIndex[minTop - 1]]) {
+    minIndex[minTop] = top;
+  } else {
+    minIndex[minTop] = minIndex[minTop - 1];
+  }
+  return OK;
 }
 
 template<class T>
 int stack<T>::popMinIndex()
 {
-    minTop--;
-    return OK;
+  minTop--;
+  return OK;
 }
 
 template<class T>
 T stack<T>::min()
 {
-    assert(minTop >= 0);
-    return buf[minIndex[minTop]];
+  assert(minTop >= 0);
+  return buf[minIndex[minTop]];
 }
 template<class T>
 stack<T>::stack(const stack<T>& s):top(s.top), minTop(s.minTop)
 {
-    top = s.top;
-    minTop = s.minTop;
-    for (int i = 0; i <= top; i++) {
-        buf[i] = s.buf[i];
-    }
-    for (int i = 0; i <= minTop; i++) {
-        minIndex[i] = s.minIndex[i];
-    }
+  top = s.top;
+  minTop = s.minTop;
+  for (int i = 0; i <= top; i++) {
+    buf[i] = s.buf[i];
+  }
+  for (int i = 0; i <= minTop; i++) {
+    minIndex[i] = s.minIndex[i];
+  }
 }
 
 template<class T>
 stack<T>& stack<T>::operator=(const stack<T>& s)
 {
-    top = s.top;
-    minTop = s.minTop;
-    for (int i = 0; i <= top; i++) {
-        buf[i] = s.buf[i];
-    }
-    for (int i = 0; i <= minTop; i++) {
-        minIndex[i] = s.minIndex[i];
-    }
-    return *this;
+  top = s.top;
+  minTop = s.minTop;
+  for (int i = 0; i <= top; i++) {
+    buf[i] = s.buf[i];
+  }
+  for (int i = 0; i <= minTop; i++) {
+    minIndex[i] = s.minIndex[i];
+  }
+  return *this;
 }
 #endif
